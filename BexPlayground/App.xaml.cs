42a42568aaf067277878f0a0ba207ab27cc98471
Scott Lovegrove <scottisafool@live.co.uk>
2015-07-09 23:22:43 +0100

    Added playground app

---
+++ BexPlayground/App.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Globalization;
+using Windows.Security.Authentication.Web;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using Bex;
+using Bex.Helpers;
+using Newtonsoft.Json;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace BexPlayground
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : Application
+    {
+        public const string ClientSecret = "Pjz88gislwxPvBfeDn8fxYimbWw3fsbV";
+        public const string ClientId = "000000004015C60E";
+
+        private TransitionCollection transitions;
+
+        public static IBexClient BexClient { get; set; }
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += this.OnSuspending;
+
+            BexClient = new BexClient(ClientSecret, ClientId);
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                // TODO: change this value to a cache size that is appropriate for your application
+                rootFrame.CacheSize = 1;
+
+                // Set the default language
+                rootFrame.Language = ApplicationLanguages.Languages[0];
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // Removes the turnstile navigation for startup.
+                if (rootFrame.ContentTransitions != null)
+                {
+                    this.transitions = new TransitionCollection();
+                    foreach (var c in rootFrame.ContentTransitions)
+                    {
+                        this.transitions.Add(c);
+                    }
+                }
+
+                rootFrame.ContentTransitions = null;
+                rootFrame.Navigated += this.RootFrame_FirstNavigated;
+
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        protected override async void OnActivated(IActivatedEventArgs args)
+        {
+            if (args?.Kind == ActivationKind.WebAuthenticationBrokerContinuation)
+            {
+                var brokerArgs = args as IWebAuthenticationBrokerContinuationEventArgs;
+                if (brokerArgs?.WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
+                {
+                    var url = new Uri(brokerArgs.WebAuthenticationResult.ResponseData);
+                    if (BexHelper.IsValidReturnUrl(url))
+                    {
+                        var code = BexHelper.ExtractCode(url);
+
+                        var creds = await BexClient.ExchangeCodeAsync(code);
+
+                        var data = ApplicationData.Current.LocalSettings;
+                        if (data.Containers.ContainsKey("Credentials"))
+                        {
+                            var container = data.Containers["Credentials"];
+                            container.Values["Credentials"] = JsonConvert.SerializeObject(creds);
+                        }
+                        else
+                        {
+                            var container = data.CreateContainer("Credentials", ApplicationDataCreateDisposition.Always);
+                            container.Values["Credentials"] = JsonConvert.SerializeObject(creds);
+                        }
+                    }
+                }
+            }
+
+            base.OnActivated(args);
+        }
+
+        /// <summary>
+        /// Restores the content transitions after the app has launched.
+        /// </summary>
+        /// <param name="sender">The object where the handler is attached.</param>
+        /// <param name="e">Details about the navigation event.</param>
+        private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e)
+        {
+            var rootFrame = sender as Frame;
+            rootFrame.ContentTransitions = this.transitions ?? new TransitionCollection() { new NavigationThemeTransition() };
+            rootFrame.Navigated -= this.RootFrame_FirstNavigated;
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+
+            // TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ BexPlayground/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+using Bex;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace BexPlayground
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (App.BexClient.Credentials == null)
+            {
+                var data = ApplicationData.Current.LocalSettings;
+                if (data.Containers.ContainsKey("Credentials"))
+                {
+                    var creds = data.Containers["Credentials"];
+                    var json = (string)creds.Values["Credentials"];
+                    if (!string.IsNullOrEmpty(json))
+                    {
+                        var deets = JsonConvert.DeserializeObject<LiveIdCredentials>(json);
+
+                        App.BexClient.SetCredentials(deets);
+                    }
+                }
+            }
+        }
+
+        private async void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (App.BexClient.Credentials != null)
+            {
+                App.BexClient.ClearCredentials();
+                return;
+            }
+
+            var url = App.BexClient.CreateAuthenticationUrl(new List<Scope>
+            {
+                Scope.ActivityHistory,
+                Scope.ActivityLocation,
+                Scope.Devices,
+                Scope.Profile
+            });
+
+            WebAuthenticationBroker.AuthenticateAndContinue(new Uri(url), new Uri(BexClient.RedirectUri), new ValueSet(), WebAuthenticationOptions.None);
+        }
+
+        private async void ProfileButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var profile = await App.BexClient.GetProfileAsync();
+            if (profile != null)
+            {
+                var dialog = new MessageDialog("Signed in as " + profile.FirstName);
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void DevicesButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var devices = await App.BexClient.GetDevicesAsync();
+            if (devices != null)
+            {
+                var dialog = new MessageDialog($"User has {devices.Count} Devices");
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
